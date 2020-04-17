67914994a140c0635267808bcc4cf59dafc3c1bc
JanethL <43326723+JanethL@users.noreply.github.com>
2020-04-17 10:36:42 -0700

    Update list.js

--- functions/events/slack/command/list.js
+++ functions/events/slack/command/list.js
@@ -1,12 +1,17 @@
-const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
+const lib = require('lib')({
+  token: process.env.STDLIB_SECRET_TOKEN
+});
 /**
-* An HTTP endpoint that acts as a webhook for Slack command event
-* @param {object} event
-* @returns {object} result Your return value
-*/
+ * An HTTP endpoint that acts as a webhook for Slack command event
+ * @param {object} event
+ * @returns {object} result Your return value
+ */
 module.exports = async (event) => {
   // Store API Responses
-const result = {googlesheets: {}, slack: {}};
+  const result = {
+    googlesheets: {},
+    slack: {}
+  };
   console.log(`Running [Slack → Retrieve Channel, DM, or Group DM by id]...`);
   result.slack.channel = await lib.slack.conversations['@0.2.5'].info({
     id: `${event.channel_id}`
@@ -23,13 +28,13 @@ const result = {googlesheets: {}, slack: {}};
       'count': 0,
       'offset': 0
     }
-});
+  });
 
-var messageArray = [];
-for (var i = 0; i < result.googlesheets.selectQueryResult.rows.length; i++) {
-  messageArray.push(`${result.googlesheets.selectQueryResult.rows[i].fields.Name}: ${result.googlesheets.selectQueryResult.rows[i].fields.BirthdayMonth}-${result.googlesheets.selectQueryResult.rows[i].fields.BirthdayDay}`)
-}
-var messageText = messageArray.join('\n');
+  var messageArray = [];
+  for (var i = 0; i < result.googlesheets.selectQueryResult.rows.length; i++) {
+    messageArray.push(`${result.googlesheets.selectQueryResult.rows[i].fields.Name}: ${result.googlesheets.selectQueryResult.rows[i].fields.BirthdayMonth}-${result.googlesheets.selectQueryResult.rows[i].fields.BirthdayDay}`)
+  }
+  var messageText = messageArray.join('\n');
 
   console.log(`Running [Slack → Create a new Ephemeral Message from your Bot]...`);
   await lib.slack.messages['@0.5.11'].ephemeral.create({
@@ -39,7 +44,7 @@ var messageText = messageArray.join('\n');
     attachments: [],
     blocks: [],
     as_user: false
-})
+  })
 
   return result;
 };
